bb207546d4d74ccb72feb30c0a77d6f7dd44c82b
EDCA <eric.cavanzo@outlook.com>
2025-06-25 17:35:06 -0500

    Obtener y escuchar servidor

--- src/class/index.ts
+++ src/class/index.ts
@@ -1,1 +1,13 @@
-console.log("Holaa");
+import { express } from "../modules/express";
+
+const app = express();
+
+const port : number = 3500;
+
+app.get('/', (request : any , response : any ) => {
+	response.send('Hola mi server en express');
+})
+
+app.listen(port, () => {
+	console.log('Mi port ', port)
+});

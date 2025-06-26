48b36bb2306423370c0e120ed79eb7537050de4b
EDCA <eric.cavanzo@outlook.com>
2025-06-26 13:56:21 -0500

    cambio de tipos y endpoint de categorias mas detallado

--- src/class/index.ts
+++ src/class/index.ts
@@ -1,40 +1,63 @@
 import { express } from "../modules/express";
-
+import  {Request, Response} from 'express'
 const app = express();
 
 const port : number = 3000;
 
-app.get('/inicio', (request : any , response : any ) => {
+app.get('/inicio', (request : Request , response : Response ) => {
 	response.send('Hola este es el inicio del servidor');
 });
 
 
-app.get('/nueva-ruta', (request : any , response : any ) => {
+app.get('/nueva-ruta', (request : Request , response : Response ) => {
 	response.send('Hola soy una nueva ruta');
 });
 
-app.get('/categorias', (request : any , response : any) => {
+app.get('/categories/:categoryId/products/:productId', (request : Request , response : Response) => {
+	const {categoryId, productId} = request.params
+
 	response.json({
-		deportes : [
-			"voleibol",
-			"Basketball",
-			"Futbol",
-			"Tenis",
-		],
-		electrodomesticos : [
-			"relojes de pulso",
-			"relojes digitales",
-			"medidores de pulso de pecho"
-		]
+		categoryId,
+		productId
 	})
+
+								// response.json({
+								// 	deportes : [
+								// 		"voleibol",
+								// 		"Basketball",
+								// 		"Futbol",
+								// 		"Tenis",
+								// 	],
+								// 	electrodomesticos : [
+								// 		"relojes de pulso",
+								// 		"relojes digitales",
+								// 		"medidores de pulso de pecho"
+								// 	]
+								// })
 });
 
-app.get('/products', (request : any , response : any ) => {
-	response.json({
-		name : 'Product',
-		price : 1000
-	});
+app.get('/products', (request : Request , response : Response ) => {
+	response.json([
+		{
+			name : 'Product 1',
+			price : 1000
+		},
+		{
+			name : 'Product 2',
+			price : 2000
+		}
+	]);
 });
+
+app.get('/products/:id', (request : Request, response : Response) => {
+	const { id } = request.params
+
+	response.json({
+		id,
+		name : 'Product 2',
+		price : 2000
+	});
+})
 
 
 app.listen(port, () => {

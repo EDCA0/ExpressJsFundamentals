98460a31d507b1f5bee5001ca898715844aa0c8c
EDCA <eric.cavanzo@outlook.com>
2025-06-26 12:58:33 -0500

    end-points de inicio, pruebas, categoria y productos

--- src/class/index.ts
+++ src/class/index.ts
@@ -2,11 +2,40 @@ import { express } from "../modules/express";
 
 const app = express();
 
-const port : number = 3500;
+const port : number = 3000;
 
-app.get('/', (request : any , response : any ) => {
-	response.send('Hola mi server en express');
-})
+app.get('/inicio', (request : any , response : any ) => {
+	response.send('Hola este es el inicio del servidor');
+});
+
+
+app.get('/nueva-ruta', (request : any , response : any ) => {
+	response.send('Hola soy una nueva ruta');
+});
+
+app.get('/categorias', (request : any , response : any) => {
+	response.json({
+		deportes : [
+			"voleibol",
+			"Basketball",
+			"Futbol",
+			"Tenis",
+		],
+		electrodomesticos : [
+			"relojes de pulso",
+			"relojes digitales",
+			"medidores de pulso de pecho"
+		]
+	})
+});
+
+app.get('/products', (request : any , response : any ) => {
+	response.json({
+		name : 'Product',
+		price : 1000
+	});
+});
+
 
 app.listen(port, () => {
 	console.log('Mi port ', port)

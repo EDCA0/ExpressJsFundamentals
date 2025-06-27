5168c8cf7bd5b0ce8575143882e9cc7f838ccc59
EDCA <eric.cavanzo@outlook.com>
2025-06-26 19:01:02 -0500

    muestra de prueba con faker para listado de productos con imagen, nombre y precio, listado de users con validaciones(uso de limit y offset con parametros query)

--- src/class/index.ts
+++ src/class/index.ts
@@ -1,5 +1,7 @@
 import { express } from "../modules/express";
 import  {Request, Response} from 'express'
+import { faker } from "../modules/faker";
+
 const app = express();
 
 const port : number = 3000;
@@ -15,39 +17,31 @@ app.get('/nueva-ruta', (request : Request , response : Response ) => {
 
 app.get('/categories/:categoryId/products/:productId', (request : Request , response : Response) => {
 	const {categoryId, productId} = request.params
-
 	response.json({
 		categoryId,
 		productId
 	})
 
-								// response.json({
-								// 	deportes : [
-								// 		"voleibol",
-								// 		"Basketball",
-								// 		"Futbol",
-								// 		"Tenis",
-								// 	],
-								// 	electrodomesticos : [
-								// 		"relojes de pulso",
-								// 		"relojes digitales",
-								// 		"medidores de pulso de pecho"
-								// 	]
-								// })
 });
 
 app.get('/products', (request : Request , response : Response ) => {
-	response.json([
-		{
-			name : 'Product 1',
-			price : 1000
-		},
-		{
-			name : 'Product 2',
-			price : 2000
-		}
-	]);
+	const products = [];
+	const limit : number = parseInt(request.query.size as string, 10) || 10;
+
+	for (let index = 0; index < limit; index++) {
+		products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url()})
+	}
+
+	response.json(products);
 });
+
+app.get('/products/filter', (request : Request, response : Response) => {
+	response.send('Yo soy un filter');
+})
+
 
 app.get('/products/:id', (request : Request, response : Response) => {
 	const { id } = request.params
@@ -57,6 +51,31 @@ app.get('/products/:id', (request : Request, response : Response) => {
 		name : 'Product 2',
 		price : 2000
 	});
+})
+
+app.get('/users', (request : Request, response : Response) => {
+	const limit : number = parseInt(request.query.limit as string, 10);
+	const offset : number = parseInt(request.query.offset as string, 10);
+	if(limit && offset) {
+		if(isNaN(limit) || isNaN(offset)) {
+			return response.status(400).json( {
+				error: 'Limit y offset deben ser numeros'
+			});
+		}
+		if(limit >= 0 && offset >= 0){
+			response.json({
+				limit,
+				offset
+			});
+		} else {
+			return response.status(400).json( {
+				error: 'Limit y offset deben ser mayores o iguales a 0'
+			});
+		}
+
+	} else {
+			response.send('No hay parametros');
+		}
 })
 
 
